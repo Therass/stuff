e1e101d3baa9675a727088e571e517f08a2e2da7
therass <therass@mail.ru>
2020-03-11 00:12:18 +0400

    still trying to remove .idea files

--- src/test/java/InPoint.java
+++ src/test/java/InPoint.java
@@ -4,6 +4,6 @@ public class InPoint implements Cloneable {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
-        //workspace brunch
+        //workspace branch
     }
 }
